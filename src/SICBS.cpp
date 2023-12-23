2dd578c5adc73410874334eae2be5a35bf9ae1b0
SimJoonYeol <antihero1290@gmail.com>
2023-12-23 16:28:39 +0900

    Fix bugs in find conflicts

--- src/SICBS.cpp
+++ src/SICBS.cpp
@@ -58,7 +58,6 @@ double SICBS::calculateCost(const Solution& solution) {
 }
 
 void SICBS::findConflicts(const Solution& solution, vector<Conflict>& conflicts) const {
-  // TODO: bug should be fixed
   for (int agent1_id = 0; agent1_id < env.num_of_robots; ++agent1_id) {
     for (int agent2_id = agent1_id + 1; agent2_id < env.num_of_robots; ++agent2_id) {
       assert(agent1_id != agent2_id);
@@ -74,26 +73,26 @@ void SICBS::findConflicts(const Solution& solution, vector<Conflict>& conflicts)
       auto [prev_point2, prev_time2] = solution[agent2_id][index2];
       auto [next_point2, next_time2] = solution[agent2_id][index2 + 1];
 
-      const double min_path_time = min(get<1>(solution[agent1_id].back()), get<1>(solution[agent2_id].back()));
+      const double max_path_time = max(get<1>(solution[agent1_id].back()), get<1>(solution[agent2_id].back()));
 
       bool is_safe = true;
       double collision_start_time = 0.0;
 
-      const auto times = static_cast<int>(floor(min_path_time / env.velocities[agent1_id])) + 1;
+      const auto times = static_cast<int>(floor(max_path_time / env.velocities[agent1_id])) + 1;
 
       for (int time = 0; time < times; ++time) {
         // update prev_point1, next_point1, prev_point2, next_point2
         if (time >= next_time1) {
           index1++;
-          prev_point1 = next_point1;
-          prev_time1 = next_time1;
+          prev_point1 = get<0>(solution[agent1_id][min(static_cast<int>(solution[agent1_id].size()) - 1, index1)]);
+          prev_time1 = get<1>(solution[agent1_id][min(static_cast<int>(solution[agent1_id].size()) - 1, index1)]);
           next_point1 = get<0>(solution[agent1_id][min(static_cast<int>(solution[agent1_id].size()) - 1, index1 + 1)]);
           next_time1 = get<1>(solution[agent1_id][min(static_cast<int>(solution[agent1_id].size()) - 1, index1 + 1)]);
         }
         if (time >= next_time2) {
           index2++;
-          prev_point2 = next_point2;
-          prev_time2 = next_time2;
+          prev_point2 = get<0>(solution[agent2_id][min(static_cast<int>(solution[agent2_id].size()) - 1, index2)]);
+          prev_time2 = get<1>(solution[agent2_id][min(static_cast<int>(solution[agent2_id].size()) - 1, index2)]);
           next_point2 = get<0>(solution[agent2_id][min(static_cast<int>(solution[agent2_id].size()) - 1, index2 + 1)]);
           next_time2 = get<1>(solution[agent2_id][min(static_cast<int>(solution[agent2_id].size()) - 1, index2 + 1)]);
         }
@@ -105,13 +104,17 @@ void SICBS::findConflicts(const Solution& solution, vector<Conflict>& conflicts)
 
         auto agent1_point = prev_point1;
         auto agent2_point = prev_point2;
+        const auto agent1_expand_time = time - prev_time1;
+        const auto agent2_expand_time = time - prev_time2;
         if (agent1_theta != 0.0) {
-          agent1_point = make_tuple(get<0>(prev_point1) + env.velocities[agent1_id] * cos(agent1_theta) * time,
-                                    get<1>(prev_point1) + env.velocities[agent1_id] * sin(agent1_theta) * time);
+          agent1_point =
+              make_tuple(get<0>(prev_point1) + env.velocities[agent1_id] * cos(agent1_theta) * agent1_expand_time,
+                         get<1>(prev_point1) + env.velocities[agent1_id] * sin(agent1_theta) * agent1_expand_time);
         }
         if (agent2_theta != 0.0) {
-          agent2_point = make_tuple(get<0>(prev_point2) + env.velocities[agent2_id] * cos(agent2_theta) * time,
-                                    get<1>(prev_point2) + env.velocities[agent2_id] * sin(agent2_theta) * time);
+          agent2_point =
+              make_tuple(get<0>(prev_point2) + env.velocities[agent2_id] * cos(agent2_theta) * agent2_expand_time,
+                         get<1>(prev_point2) + env.velocities[agent2_id] * sin(agent2_theta) * agent2_expand_time);
         }
 
         if (calculateDistance(agent1_point, agent2_point) < env.radii[agent1_id] + env.radii[agent2_id]) {
